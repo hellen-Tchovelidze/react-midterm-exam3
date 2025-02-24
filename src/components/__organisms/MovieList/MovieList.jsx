b61a7128964b240e1eb1c05d12ee244ac8ad0385
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-02-24 20:36:23 +0400

    fixed(change scr)

--- src/components/__organisms/MovieList/MovieList.jsx
+++ src/components/__organisms/MovieList/MovieList.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 import data from "../../../data.json";
- 
 
 const MovieList = () => {
   return (
     <div className=" flex flex-wrap">
-     
       {data.map((movie, index) => (
         <div key={index} className=" flex flex-col w-[280px] h-[226px]">
-          
           <img src={movie.thumbnail.regular.small} alt={movie.title} />
-          <div>
-            <p className=' text-white'>{movie.year}</p>
-                <p className=' text-white'>{movie.category}</p>
+
+          <div className=" flex w-[156px] justify-between items-start">
+            <p className=" text-white">{movie.year}</p>
+            <p className=" text-white">{movie.category}</p>
+            <p className=" text-white">{movie.rating}</p>
           </div>
-          <h1 className=' text-white'>{movie.title}</h1>
+          <h1 className=" text-white">{movie.title}</h1>
         </div>
       ))}
     </div>
